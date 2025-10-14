4fac812efe02533637d09a74de4064177e6ff36b
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-10-14 15:03:53 -0300

    implementado swagger e documentando usuario controller

--- src/main/java/com/fiap/giedapi/controller/UsuarioController.java
+++ src/main/java/com/fiap/giedapi/controller/UsuarioController.java
@@ -8,6 +8,9 @@ import com.fiap.giedapi.dto.request.UserRequestDTO;
 import com.fiap.giedapi.dto.response.UserResponseDTO;
 import com.fiap.giedapi.exception.EntityNotFoundException;
 import com.fiap.giedapi.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +25,15 @@ public class UsuarioController {
     @Autowired
     private UsuarioService service;
 
+    @Operation(summary = "Cria um novo usuário" ,description = "Endpoint para" +
+            " cadastrar um novo usuário no sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Campos " +
+                    "preenchidos incorretamente"),
+            @ApiResponse(responseCode = "409", description = "Um usuário já " +
+                    "existe com esses dados")
+    })
     @PostMapping
     public ResponseEntity<UserResponseDTO> criarUsuario (@RequestBody UserRequestDTO userRequest){
         Usuario user = UserMapper.toUser(userRequest);
@@ -32,6 +44,14 @@ public class UsuarioController {
         return ResponseEntity.created(URI.create("/api/user/"+responseDTO.id())).body(responseDTO);
     }
 
+    @Operation(summary = "Buscar Usuario" , description = "Endpoint " +
+            "que busca o usuário pelo id fornecido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200" , description = "Retorna o " +
+                    "usuário"),
+            @ApiResponse(responseCode = "404", description = "Usuário com id " +
+                    "inexistente")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> buscarPorId(@PathVariable Long id){
         try{
@@ -43,12 +63,26 @@ public class UsuarioController {
 
     }
 
+    @Operation(summary = "Deleta um usuário", description = "Deleta um " +
+            "usuário pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Usuário " +
+                    "deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário com id " +
+                    "não encontrado")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id){
         boolean removido = service.deletarUsuario(id);
         return removido ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 
+    @Operation(summary="Alterar senha do usuário", description = "Altera a " +
+            "senha do usuario pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Senha alterada " +
+                    "com sucesso.")
+    })
     @PutMapping("/alterar-senha/{id}")
     public ResponseEntity<Void> alterarSenha(@PathVariable Long id,
                                              @RequestBody SenhaRequestDTO dto){
@@ -56,6 +90,14 @@ public class UsuarioController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Login", description = "Endpoint para realizar login" +
+            " do usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login realizado" +
+                    " com sucesso"),
+            @ApiResponse(responseCode = "401", description = "Crendenciais " +
+                    "incorretas")
+    })
     @PostMapping("/login")
     public ResponseEntity<UserResponseDTO> login(@RequestBody LoginRequestDTO dto) {
         try {
